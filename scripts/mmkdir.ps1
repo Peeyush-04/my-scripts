4d2e2c236105910ea3a7da37d383d4ace1e34d2a
Peeyush Maurya <peeyushmaurya.dev@gmail.com>
2025-07-06 22:40:15 +0530

    Included `muti-mkdir` script logic

---
+++ scripts/mmkdir.ps1
@@ -0,0 +1,10 @@
+param (
+  [Parameter(ValueFromRemainingArguments = $true)]
+  [string[]]$Folders
+)
+
+# path variable mkdir.exe from build
+$exePath = Resolve-Path "$PSScriptRoot\..\build\mmkdir.exe"
+
+# execute mkdir program
+& $exePath @Folders
