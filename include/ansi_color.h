d74c86c91f9634341f740c14fff2043598eaa852
Peeyush Maurya <peeyushmaurya.dev@gmail.com>
2025-07-06 20:31:22 +0530

    Included ANSI encoded color global variables

---
+++ include/ansi_color.h
@@ -0,0 +1,21 @@
+#ifndef ANSI_COLOR_H
+#define ANSI_COLOR_H
+
+// white color
+#define WHITE "\x1b[0m"
+#define BOLD_WHITE "\x1b[1;0m"
+
+// colors
+#define BLACK       "\x1b[30m"
+#define RED         "\x1b[31m"
+#define GREEN       "\x1b[32m"
+#define YELLOW      "\x1b[33m"
+#define BLUE        "\x1b[34m"
+
+// bold colors
+#define BOLD_RED    "\x1b[1;31m"
+#define BOLD_GREEN  "\x1b[1;32m"
+#define BOLD_YELLOW "\x1b[1;33m"
+#define BOLD_BLUE   "\x1b[1;34m"
+
+#endif
