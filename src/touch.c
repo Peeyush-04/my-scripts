55aed40df153382da32a4078c436ffe6aed704f7
Peeyush Maurya <peeyushmaurya.dev@gmail.com>
2025-07-06 20:01:33 +0530

    Included color change feature using windows.h library

--- src/touch.c
+++ src/touch.c
@@ -2,10 +2,23 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <windows.h>
+
+#define setColorGreen(handleName) SetConsoleTextAttribute(handleName, FOREGROUND_GREEN | FOREGROUND_INTENSITY)
+#define setColorWhite(handleName) SetConsoleTextAttribute(handleName, FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_INTENSITY)
 
 int main(int argc, char *argv[]) {
+  // color change logic
+  // get handle from STD_HANDLE_OUTPUT
+  HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
   // creating files
-  printf("\nSuccessfully executed.\n");
+  printf("\n=> ");
+  // color => green
+  setColorGreen(hConsole);
+  printf("Executing...\n");
+  setColorWhite(hConsole);
+  
   for(int i = 1; i < argc; i++) {
     int fd = open(argv[i], O_CREAT | O_WRONLY, 0644);
     
@@ -16,9 +29,17 @@ int main(int argc, char *argv[]) {
     }
 
     close(fd);
-    printf("Created: %s\n", argv[i]);
+
+    printf("=> ");
+    // set color to green
+    setColorGreen(hConsole);
+    printf("Created:");
+    setColorWhite(hConsole);
+    printf(" %s\n", argv[i]);
   }
   printf("\n");
 
+  // successful message
+  printf("Program Executed successfully!\n");
   return 0; // no issues
 }
