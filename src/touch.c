fcf0914746afe44d0fb0a12637c209aebb750b31
Peeyush Maurya <peeyushmaurya.dev@gmail.com>
2025-07-06 20:31:51 +0530

    Updated touch.c based on include/ansi_color.h

--- src/touch.c
+++ src/touch.c
@@ -2,15 +2,11 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-
-// ANSI version (easy to understand)
-#define RED   "\x1b[31m"
-#define GREEN "\x1b[32m"
-#define WHITE "\x1b[0m"
+#include "../include/ansi_color.h"
 
 int main(int argc, char *argv[]) {
   // creating files
-  printf(WHITE "\n=> " GREEN "Executing...\n" WHITE);
+  printf(WHITE "\n=> " BOLD_GREEN "Executing...\n" WHITE);
   for(int i = 1; i < argc; i++) {
     int fd = open(argv[i], O_CREAT | O_WRONLY, 0644);
     
@@ -25,6 +21,6 @@ int main(int argc, char *argv[]) {
   }
   printf("\n");
 
-  printf("Program Executed successfully!\n");
+  printf(BOLD_WHITE "Program Executed successfully!\n" WHITE);
   return 0; // no issues
 }
